534a6fdfd3ff04a8ed1b8f90d17972a205271bb4
gouravtiwari2004 <gouravtiwari111213@gmail.com>
2023-02-25 21:35:32 +0530

    day-5-Gourav

--- Cart Page/Cart.js
+++ Cart Page/Cart.js
@@ -1,28 +1,109 @@
+let CartData = JSON.parse(localStorage.getItem("CartData")) || [];
+let displayCart = document.getElementById("displayCart");
 
-let CartData = JSON.parse(localStorage.getItem("CartData"))||[]
-let MainContainer = document.getElementById("MainContainer")
+function renderData() {
+  displayCart.innerHTML = null;
+  let cardList = `
+       ${CartData.map((element) =>
+         getCard(
+           element.productType,
+           element.size,
+           element.quantity,
+           element.Stock,
+           element.id,
+           element.image1,
+           element.image2,
+           element.image3,
+           element.image4,
+           element.title,
+           element.price,
+           element.color,
+           element.category,
+           element.rating,
+           element.description1
+         )
+       ).join("")}
+  `;
+  displayCart.innerHTML = cardList;
 
-    let cardList = `
-    
+  let TotalBill = document.getElementById("TotalBill");
+  let totalsaving = document.getElementById("totalsaving");
+  let totalProduct = document.getElementById("totalProduct");
 
-    <div id="MainContainer">
-         ${CartData.map((element)=>  getCard(element.productType,element.size,element.quantity,element.Stock,element.id,element.image1,element.image2,element.image3,element.image4,element.title,element.price,element.color,element.category,element.rating,element.description1)).join("")}
-      </div>
-    `
-   MainContainer.innerHTML = cardList
+  totalProduct.innerText = CartData.length;
 
+  let sum = 0;
+  let discount = 0;
+  for (let i = 0; i < CartData.length; i++) {
+    sum += CartData[i].price * CartData[i].quantity;
+    discount += (CartData[i].price * CartData[i].quantity) / 12;
+  }
 
+  TotalBill.innerText = sum;
+  totalsaving.innerText = Math.floor(discount);
 
+  // increment btn
+
+  let increment = document.getElementsByClassName("increment");
+  for (let inc of increment) {
+    inc.addEventListener("click", (e) => {
+      for (let key of CartData) {
+        if (key.id === e.target.dataset.id) {
+          key.quantity = key.quantity + 1;
+        }
+      }
+      localStorage.setItem("CartData", JSON.stringify(CartData));
+      renderData();
+    });
+  }
+
+  //  decrement button
+  let dicrement = document.getElementsByClassName("dicrement");
+  for (let inc of dicrement) {
+    inc.addEventListener("click", (e) => {
+      e.preventDefault();
+      for (let key of CartData) {
+        if (key.id === e.target.dataset.id) {
+          let value = key.quantity;
+          if (value <= 1) {
+            value = 1;
+            key.quantity = value;
+          } else {
+            value--;
+            key.quantity = value;
+          }
+        }
+      }
+      localStorage.setItem("CartData", JSON.stringify(CartData));
+      renderData();
+    });
+  }
+}
+renderData();
 
 //  rendering the data whic we are getting from add to cart
 
-function getCard(productType,size,quantity,Stock,id,image1,image2,image3,image4,title,price,color,category,rating,description1){
-  
-   
-    let card = `
-    <div id="displayCart">
-    <div class="productLeft">
-    <div class="imgdiv"><img src=${image1} alt=""></div>
+function getCard(
+  productType,
+  size,
+  quantity,
+  Stock,
+  id,
+  image1,
+  image2,
+  image3,
+  image4,
+  title,
+  price,
+  color,
+  category,
+  rating,
+  description1
+) {
+  let card = `
+  <div>
+     <div class="productLeft">
+     <div class="imgdiv"><img src=${image1} alt=""></div>
     <div class="productdetailsdiv">
       <p>QUEENS CLOEST</p>
       <p class="title">${title}</p>
@@ -48,9 +129,9 @@ function getCard(productType,size,quantity,Stock,id,image1,image2,image3,image4,
     <!-- quantity -->
     <div class="quantity">
        <div>
-        <div   data-id=${id} class="dicrement" id="dicrement">-</div>
-         <div  data-id=${id} id="Quant">${quantity}</div>
-          <div data-id=${id} class="increment" id="increment">+</div>
+        <div   data-id=${id}  class="dicrement" id="dicrement">-</div>
+         <div  data-id=${id}  class="Quant" id="Quant">${quantity}</div>
+          <div data-id=${id}  class="increment" id="increment">+</div>
           
        </div>
     </div>
@@ -71,76 +152,13 @@ function getCard(productType,size,quantity,Stock,id,image1,image2,image3,image4,
     </div>
   </div>
   </div>
-  `   
- 
-    return card
-    
-   
+  `;
+
+  return card;
 }
 
-// let ndprice = document.querySelectorAll(".ndprice")
-let TotalBill = document.getElementById("TotalBill")
-let totalsaving =document.getElementById("totalsaving")
-let totalProduct = document.getElementById("totalProduct")
-// sum 
-let discount = 0
-let sum = 0
-for (let i = 0 ; i< CartData.length ; i++){
-    sum+= +CartData[i].price * CartData[i].quantity
-    discount += +CartData[i].price * CartData[i].quantity/12
-}
 
-//  increment
-
-
-// let 
-let PRICE = document.getElementById("ndprice")
-totalProduct.innerText = CartData.length
-TotalBill.innerText = sum
-totalsaving.innerText = Math.floor(discount)
-let Quant = document.getElementById("Quant")
-
-let increment = document.querySelectorAll(".increment")
-for (let inc of increment){
-   inc.addEventListener("click",(e)=> {
-   Quant.innerText++
-   totalProduct.innerText++
-   TotalBill.innerText = sum+ (+PRICE.innerText*Quant.innerText)
-   discount = TotalBill.innerText /12
-   totalsaving.innerText = Math.floor(discount)
-   
-  })
-}
-
-let dicrement = document.querySelectorAll(".dicrement")
-for (let inc of dicrement){
-   inc.addEventListener("click",(e)=> {
-   Quant.innerText--
-   totalProduct.innerText--
-   TotalBill.innerText = sum+ (+PRICE.innerText*Quant.innerText)
-   discount = TotalBill.innerText /12
-   totalsaving.innerText = Math.floor(discount)
- })
-}
-
-// checkout btn 
 let checkoutBtn = document.getElementById("checkoutBtn")
-  checkoutBtn.addEventListener("click",(e)=>{
-   localStorage.setItem("CheckoutData",JSON.stringify(CartData))
-  //  location.href = "CheckoutPage.html"
+checkoutBtn.addEventListener("click",()=> {
+  location.href="/CheckoutPage/checkoutPage.html"
 })
-
-
-// remove Btn
-
-let removeBtn = document.querySelectorAll(".remove")
-for (let remove of removeBtn) {
-  remove.addEventListener("click",(e)=>{
-    e.preventDefault()
-    CartData.forEach(element => {
-     if(CartData.id == element.id){
-      log
-     }
-    });
-  })
-}
